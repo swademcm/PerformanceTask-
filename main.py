2c9e7597053e4e105beb2a7e42512869d9a30c05
swade <156927269+swademcm@users.noreply.github.com>
2024-03-06 11:13:36 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,49 @@
 import time, os
+
 os.system('cls')
+# clear the screen
 input('Welcome to Fly The Ship, \nFly your ship and gain coins depending on how high you go. Use these coins to upgrade your ship how you like and fly even higher.\n\nTry and get as high as you can! Press Enter to Get Started.\n')
+# tutorial / intro
+
 os.system('cls')
+# clear the screen
 name = input('What is your name?\n')
-os.system('cls')
-print(".")
 
-time.sleep(0.5)
-os.system('cls')
-print("..")
-time.sleep(0.5)
-os.system('cls')
-print("...")
-time.sleep(1)
 
-os.system('cls')
-print(f'Welcome, {name}\n\n')
-input('''
+def loading(message):
+# start of loading animation
+    os.system('cls')
+    print(f"{message}.")
+    time.sleep(0.5)
+    os.system('cls')
+    print(f"{message}..")
+    time.sleep(0.5)
+    os.system('cls')
+    print(f"{message}...")
+    time.sleep(1)
+    os.system('cls')
+# end of loading animation
+
+
+
+
+def home():
+    choice = input('''
 What would you like to do?
       
 F - Fly Ship
 S - Open Shop
 Q - Quit
 ''')
+    
+
+    if choice == "F":
+        print("Taking Off")
+    if choice == "Q":
+        os.system('cls')
+        print("Thanks For Playing!")
+        os.abort()
+
+    
+loading("")  
+home()
